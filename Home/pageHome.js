9f8c7cb4facda53451f0b646a288d39c0384f6e2
Adnrew <trongtruyen2581995@gmail.com>
2023-08-11 17:43:25 +0700

    update ui and function login, sign up at page home and admin

--- Home/pageHome.js
+++ Home/pageHome.js
@@ -3,3 +3,9 @@ $(function () {
   $("#banner_home").load("./bannerHome.html");
   $("#list_product_home").load("./listProductHome.html");
 });
+function handleLogin() {
+  $("#loginModal").modal("show");
+}
+function handleSignUp() {
+  $("#signUpModal").modal("show");
+}
